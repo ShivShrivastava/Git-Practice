c70cdbcb223bd95583a6fd8129c34704f95bdfb8
ShivShrivastava <86486414+ShivShrivastava@users.noreply.github.com>
2025-07-12 18:43:08 +0530

    Add main.tf file

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "example-rg"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "example-rg1"
+  location = "East US"
+}

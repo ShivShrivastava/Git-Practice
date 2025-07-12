54fbeb88ca29cabc634fb55a1a751c99903ff8ca
ShivShrivastava <86486414+ShivShrivastava@users.noreply.github.com>
2025-07-12 20:00:55 +0530

    Add one RG

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "example-rg1"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "example-rg2"
+  location = "East US"
+}

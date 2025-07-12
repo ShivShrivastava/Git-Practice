1b726e935c45d0db6a9c0c75fcd520290685193c
ShivShrivastava <86486414+ShivShrivastava@users.noreply.github.com>
2025-07-12 19:55:37 +0530

    Remove one RG

--- main.tf
+++ main.tf
@@ -7,8 +7,3 @@ resource "azurerm_resource_group" "rg1" {
   name     = "example-rg1"
   location = "East US"
 }
-
-resource "azurerm_resource_group" "rg1" {
-  name     = "example-rg1"
-  location = "East US"
-}
